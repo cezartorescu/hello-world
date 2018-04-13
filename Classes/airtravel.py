f2cdb75344210087b96ff89616a024b430e6dff6
Cezar <runaway@runaway.ro>
2018-04-14 01:02:28 +0300

    New class airtravel

--- Classes/airtravel.py
+++ Classes/airtravel.py
@@ -2,11 +2,66 @@
 
 class Flight:
 
-    def __init__(self, number):
+    def __init__(self, number, aircraft):
         self._number = number
+        self._aircraft = aircraft
+        rows, seats = self._aircraft.seating_plan()
+        self._seating = [None] + [{letter:None for letter in seats} for row in rows]
 
     def number(self):
         return self._number
 
     def airline(self):
         return self._number[:2]
+
+    def aircraft_model(self):
+        return self._aircraft.model()
+
+    def allocate_seat(self, seat, passenger):
+        """Allocate a seat to a passenger
+        Args:
+            seat: A seat designator such as  '12C' or '21F'
+            passenger: The passenger name.
+
+        Raises:
+            ValueError: If the seat is unavailable.
+        """
+        rows, seat_letters = self._aircraft.seating_plan()
+        letter = seat[-1]
+        if letter not in seat_letters:
+            raise ValueError("Invalid seat letter {}".format(letter))
+
+        row_text = seat[:-1]
+        try:
+            row = int(row_text)
+        except ValueError:
+            raise ValueError("Invalid seat row {}".format(row_text))
+
+        if row not in rows:
+            raise ValueError("Invalid row number {}".format(row))
+
+        if self._seating[row][letter] is not None:
+            raise ValueError("Seat {} already occupied".format(seat))
+
+        self._seating[row][letter] = passenger
+
+    def num_available_seats(self):
+        return sum(sum(1 for s in row.values() if s is None) for row in self._seating if row is not None)
+
+
+class Aircraft:
+
+    def __init__(self, registration, model, num_rows, num_seats_per_row):
+        self._registration = registration
+        self._model = model
+        self._num_rows = num_rows
+        self._num_seats_per_row = num_seats_per_row
+
+    def registration(self):
+        return self._registration
+
+    def model(self):
+        return self._model
+
+    def seating_plan(self):
+        return(range(1, self._num_rows + 1), "ABCDEFGHJK"[:self._num_seats_per_row])
